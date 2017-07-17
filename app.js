d23756c1fd60837e87966b0df7ac8de9e7b1c469
mrkit <neptmanhall@gmail.com>
2017-07-16 21:57:05 -0400

    step 3

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const 
   express = require('express'),
   app = express(),
-  morgan = require('morgan');
+  morgan = require('morgan'),//shows the status
+  nunjucks = require('nunjucks'),
+  locals = {
+    topSeller: 'suicide-vest'
+  }; //to be used with nunjucks
 
+nunjucks.configure('views');
+nunjucks.render('index.html', locals, function(err, output){
+  if(err) return console.log(err);
+  console.log(output);
+});
 
 app.use(morgan('dev'));
 
@@ -14,8 +23,6 @@ app.get('/news', function(req, res, next){
   res.json({name: 'newsRoute', data: 12345});
   next();
 });
-
-
 
 app.listen(3000, function(){
   console.log('listening on port 3000');
